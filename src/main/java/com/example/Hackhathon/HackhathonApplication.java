089878ad2b45a3c5887848668502434e70cb215f
SanjayRanka2002 <samratking120@gmial.com>
2026-07-12 08:55:44 +0530

    sanjay

---
+++ src/main/java/com/example/Hackhathon/HackhathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hackhathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackhathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackhathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hackhathon/HackhathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hackhathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackhathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
